5f940d2192f2095c24dfc9987209da862eb49765
jordanSN1 <jordan.turnaco@ecoles-epsi.net>
2024-11-05 11:37:43 +0100

    init commit

---
+++ pages/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="products.php">Products</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="cart.php">Cart</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
