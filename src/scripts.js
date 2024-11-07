fc20469ec4cd336e4a06995adeb52afcd6b37590
nabilsaied15 <nabilsaied04@gmail.com>
2024-11-07 22:33:45 +0100

    page index

--- src/scripts.js
+++ src/scripts.js
@@ -5,3 +5,6 @@ const navLinks = document.getElementById('nav-links');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // Ajouter/retirer la classe 'active' pour ouvrir/fermer le menu
 });
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("nav-links").classList.toggle("show");
+});
