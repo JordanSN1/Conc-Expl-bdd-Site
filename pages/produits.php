ab5983e5c959032d1e77d2dd3c61cd27fdb61f41
nabilsaied15 <nabilsaied04@gmail.com>
2024-11-07 09:11:43 +0100

    branch nabil

--- pages/produits.php
+++ pages/produits.php
@@ -57,32 +57,6 @@
         </nav>
     </header>
 
-<style>
-<?php include("produits.css");?>
-</style>
-
-<div class="product-list">
-        <?php  
-        error_reporting(E_ALL);
-        ini_set('display_errors','1');
-        include("../scripts/conn.php");
-        
-
-        $products = $conn->query("SELECT * FROM products")->fetchAll();
-        foreach($products as $product) {
-            echo "
-                <div class='product-card'>
-                    <div class='product-image'>
-                        <img src='../img/" . htmlspecialchars($product['picture']) . "' alt='" . htmlspecialchars($product['name']) . "'>
-                    </div>
-                    <h3 class='product-title'>" . htmlspecialchars($product['name']) . "</h3>
-                    <p class='price'>" . htmlspecialchars($product['price']) . " €</p>
-                    <p class='description'>" . htmlspecialchars($product['description']) . "</p>
-                </div>
-            ";
-        }
-        ?>
-    </div>
 
     <script src="../src/scripts.js"></script>
 </body>
