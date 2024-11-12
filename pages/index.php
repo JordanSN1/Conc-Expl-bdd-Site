d3f326b81fdc0e2c9f96acd093fedad908a9834f
nabilsaied15 <nabilsaied04@gmail.com>
2024-11-12 10:16:29 +0100

    page article

--- pages/index.php
+++ pages/index.php
@@ -1,62 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/style.css">
-
-    <title>Produits - PhantomBurger</title>
-</head>
-
-<body>
-    <header>
-        <nav>
-            <div class="logo">
-                <img src="../assets/phantomBurgerlogo.png" alt="PhantomBurger logo">
-            </div>
-
-            <!-- Menu burger pour mobile -->
-            <div class="menu-toggle" id="menu-toggle">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-
-            <!-- Liens de navigation principaux -->
-            <ul class="nav-links" id="nav-links">
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="produits.php">Produits</a></li>
-                <li><a href="a-propos.php">À propos</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <!-- Liens utilisateur (se connecter/panier) visibles uniquement en version mobile -->
-                <li class="mobile-user-actions"></li>
-                </li>
-                <?php
-                session_start();
-                if (isset($_SESSION['user_id'])) {
-                    echo '<a class="mobile-panier" href="panier.php">Panier</a>';
-                } else {
-                    echo '<a class="mobile-connect" href="login.php">Se connecter</a>';
-                }
-                ?>
-                </li>
-            </ul>
-
-            <!-- Liens utilisateur (se connecter/panier) visibles uniquement en version desktop -->
-            <div class="user-actions">
-                <?php
-
-                if (isset($_SESSION['user_id'])) {
-                    echo '<a class="panier" href="panier.php">Panier</a>';
-                } else {
-                    echo '<a class="connect" href="connexion.php">connecter</a>';
-                }
-                ?>
-            </div>
-        </nav>
-    </header>
-
+<header>
+        <?php include("../header/header.php"); ?>
+</header>
     <style>
         <?php include("index.css"); ?>
     </style>
@@ -68,16 +12,19 @@
         include("../scripts/conn.php");
 
 
-        $products = $conn->query("SELECT * FROM burgers WHERE burger_id <= 6;");
+        $products = $conn->query("SELECT * FROM burgers WHERE burger_id <= 5;");
         foreach ($products as $product) {
             echo "
                 <div class='product-card'>
                     <div class='product-image'>
                         <img src='../img/" . htmlspecialchars($product['picture']) . "' alt='" . htmlspecialchars($product['name']) . "'>
                     </div>
-                    <h3 class='product-title'>" . htmlspecialchars($product['name']) . "</h3>
+                    <td><a href ='../pages/articles.php?id=".$product['burger_id']."'>".$product['name']."</a></td>
+
                     <p class='prix'>" . htmlspecialchars($product['prix']) . " €</p>
                     <p class='description'>" . htmlspecialchars($product['description']) . "</p>
+                    
+
                 </div>
             ";
         }
@@ -124,5 +71,8 @@
 
     <script src="../src/scripts.js"></script>
 </body>
+<footer>
+        <?php include("../footer/footer.php"); ?>
+    </footer>
 
 </html>
